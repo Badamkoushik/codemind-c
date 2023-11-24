865604e09c6aa3a2256e1640e8dfe50f73f89c8a
Badamkoushik <147793162+Badamkoushik@users.noreply.github.com>
2023-11-24 12:09:24 +0530

    #include<stdio.h> int main() {     int n,i,j=10;     scanf("%d",&n);     for(n=1;n<=j;n++)     {         printf("%d ",n);     } }

--- 1_to_10.c
+++ 1_to_10.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int i,j=10,n;
-    scanf("%d",&i);
-    for(n=i;n<=j;n++)
+    int n,i,j=10;
+    scanf("%d",&n);
+    for(n=1;n<=j;n++)
     {
         printf("%d ",n);
     }
