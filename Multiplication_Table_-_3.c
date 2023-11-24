b8715ea8bc5010231f78d34a48dea00f923b5fad
Badamkoushik <147793162+Badamkoushik@users.noreply.github.com>
2023-11-24 11:55:33 +0530

    #include<stdio.h> int main() {     int N,A,B;     scanf("%d%d%d",&N,&A,&B);     while(A<=B)     {         printf("%d x %d = %d ",N,A,N*A);         A++;     } }

--- Multiplication_Table_-_3.c
+++ Multiplication_Table_-_3.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int N,A=14,B;
+    int N,A,B;
     scanf("%d%d%d",&N,&A,&B);
     while(A<=B)
     {
-        
         printf("%d x %d = %d
 ",N,A,N*A);
         A++;
     }
-    
 }
